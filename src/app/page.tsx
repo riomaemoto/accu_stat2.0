783c41f86c13b82d92fb147075fc04beadf58157
rio maemoto <leo.a1025ark@gmail.com>
2025-04-28 17:23:23 +0900

    made breaks row and input section for putting name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <>sght</>;
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="flex">
+        <input
+          className="w-200px h-20px border border-gray-300 text-center"
+          placeholder="Enter your name here"
+          type="text"
+        />
+
+        <div className="border border-solid w-[70px] h-[70px] flex items-center justify-center">
+          First
+          <br />
+          Breaks
+        </div>
+        <div className="border border-solid w-[250px] h-[70px] flex items-center justify-center">
+          Breaks
+        </div>
+
+        <div className="border border-solid w-[70px] h-[70px] flex items-center justify-center">
+          Odd
+          <br />
+          Breaks
+        </div>
+        <input
+          className="w-200px h-20px border border-gray-300 text-center"
+          placeholder="Enter your name here"
+          type="text"
+        />
+      </div>
+    </div>
+  );
 }
