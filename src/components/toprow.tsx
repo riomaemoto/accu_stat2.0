97261d373409f10b6e46608defb298897f7ff50d
rio maemoto <leo.a1025ark@gmail.com>
2025-06-06 18:11:23 +0900

    feat: implement player name inputs in Toprow component to add the value to the statAtom

--- src/components/toprow.tsx
+++ src/components/toprow.tsx
@@ -1,11 +1,19 @@
+import { player1Atom, player2Atom } from "@/app/atom";
+import { useAtom } from "jotai";
+
 export const Toprow = () => {
+  const [leftName, setLeftName] = useAtom(player1Atom);
+  const [rightName, setRightName] = useAtom(player2Atom);
+
   return (
     <div className="w-full">
-      <div className="w-full max-w-[1400px] flex flex-row  items-center justify-center">
+      <div className="w-full max-w-[1400px] flex flex-row items-center justify-center">
         <input
           className="w-[140px] md:w-[150px] h-[70px] border-[1px] border-solid text-center text-[14px] md:text-[16px]"
           placeholder="Enter Name here"
           type="text"
+          onChange={(e) => setLeftName(e.target.value)}
+          value={leftName}
         />
 
         <div className="border border-solid w-[260px] md:w-[250px] h-[70px] flex items-center justify-center bg-sky-200">
@@ -16,6 +24,8 @@ export const Toprow = () => {
           className="w-[140px] md:w-[150px] h-[70px] border-[1px] border-solid text-center text-[14px] md:text-[16px]"
           placeholder="Enter Name here"
           type="text"
+          onChange={(e) => setRightName(e.target.value)}
+          value={rightName}
         />
       </div>
     </div>
